1736305b46de68b2387b2a7bbc57fbc9ef53f07b
Maxam <azam.mamatmurodov@gmail.com>
2018-03-06 22:21:20 +0500

    init

--- main/modules.py
+++ main/modules.py
@@ -2,6 +2,25 @@ from django.template.loader import render_to_string
 
 from main.models import Menu, Banner
 from products.models import Category
+from orders.models import Cart
+
+
+def get_breadcrumb(page_name, request):
+    pass
+
+
+def cart(request):
+    current_user_session_key = request.session._get_or_create_session_key()
+    cart_items = Cart.objects.filter(session_key=current_user_session_key, status=True, order__isnull=True)
+    cart_total_price = 0
+    for cart_item in cart_items:
+        cart_total_price += cart_item.total_price
+    cart_total_count = cart_items.count()
+    return {
+        'cart_items': cart_items,
+        'cart_total_price': cart_total_price,
+        'cart_total_count': cart_total_count
+    }
 
 
 def get_header(request):
@@ -9,6 +28,7 @@ def get_header(request):
     context['menu_items'] = Menu.objects.all()
     context['category'] = Category.objects.all()
     context['request'] = request
+    context['cart'] = cart(request)
     return render_to_string('parts/header.html', context=context)
 
 
