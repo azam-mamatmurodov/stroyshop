87982b1126ef15c82aede1dc13e9bb724f5edf50
Maxam <azam.mamatmurodov@gmail.com>
2018-03-24 00:26:00 +0500

    Added ProductsImportView's urls

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from products.views import ProductListView, ProductDetail, SearchView
+from products.views import ProductListView, ProductDetail, SearchView, ProductsImportView
 
 urlpatterns = [
     url(r'^category/(?P<slug>[-\w]+)/$', ProductListView.as_view(), name='product_list'),
     url(r'^category/(?P<slug>[-\w]+)/product/(?P<pk>\d+)/$', ProductDetail.as_view(), name='product_detail'),
     url(r'^search/$', SearchView.as_view(), name='search', ),
+    url(r'^products/import/$', ProductsImportView.as_view(), name='products_import', ),
 ]
